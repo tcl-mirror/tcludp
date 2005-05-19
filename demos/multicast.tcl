348013d3ebab60edc990a57a2f5f54fc878f1b23
patthoyts <patthoyts>
2005-05-19 15:41:05 +0000

    * generic/udp_tcl.c: fconfigure options were returning lists of 	lists. Fixed.

--- demos/multicast.tcl
+++ demos/multicast.tcl
@@ -5,14 +5,15 @@
 # You can send to ths using netcat:
 #  echo HELLO | nc -u 224.5.1.21 7771
 #
-# $Id: multicast.tcl,v 1.1 2004/11/22 23:48:47 patthoyts Exp $
+# $Id: multicast.tcl,v 1.2 2005/05/19 15:41:06 patthoyts Exp $
 
 package require udp 1.0.6
 
 proc udpEvent {chan} {
     set data [read $chan]
     set peer [fconfigure $chan -peer]
-    puts "$peer [string length $data] '$data'"
+    set group [lindex [fconfigure $chan -remote] 0]
+    puts "$peer ($group) [string length $data] '$data' {[fconfigure $chan]}"
     if {[string match "QUIT*" $data]} {
         close $chan
         set ::forever 1
@@ -21,13 +22,18 @@ proc udpEvent {chan} {
 }
 
 # Select a multicast group and the port number.
-set group 224.5.1.21
-set port  7771
+#
+# We have two groups here to show that it's possible.
+#
+set group1 224.5.1.21
+set group2 224.5.2.21
+set port   7771
 
 # Create a listening socket and configure for sending too.
 set s [udp_open $port]
 fconfigure $s -buffering none -blocking 0
-fconfigure $s -mcastadd $group -remote [list $group $port]
+fconfigure $s -mcastadd $group1 -remote [list $group1 $port]
+fconfigure $s -mcastadd $group2 -remote [list $group2 $port]
 fileevent $s readable [list udpEvent $s]
 
 # Announce our presence and run
